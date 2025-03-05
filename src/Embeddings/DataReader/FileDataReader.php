7c08411f5dc71a71ad69a27f76ac0c14c6fa7a88
charescape <53265646+charescape@users.noreply.github.com>
2025-03-06 00:21:23 +0800

    Update FileDataReader.php

--- src/Embeddings/DataReader/FileDataReader.php
+++ src/Embeddings/DataReader/FileDataReader.php
@@ -88,7 +88,7 @@ final class FileDataReader implements DataReader
 
 
         if (str_starts_with($file_mime_type ?? '', 'image/')) {
-            if (pf_get_config_conf('DASHSCOPE_API_KEY_4_OCR')) {
+            if (pf_get_config_conf('OCR_MODEL_API_KEY')) {
                 return (new GptOcrReader())->getText($path, $file_mime_type);
             } else {
                 try {
